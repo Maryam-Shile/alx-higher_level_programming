c52d441fe9460cb5dcea94da72587cfa5b6d8e0b
Maryam Adegboyega <maryamshile@gmail.com>
2023-08-14 05:46:21 -0700

    created 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        return
+    else:
+        new_str = ""
+        for i in my_string:
+            if i != 'c' and i != 'C':
+                new_str += i
+        return new_str
