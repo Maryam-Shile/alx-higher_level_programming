0fc66d96a974bd72c74987878d61f9bbc2ff0df1
Maryam Adegboyega <maryamshile@gmail.com>
2023-08-16 22:17:23 -0700

    4-only_diff_elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_elements = set()
-    for element in set_1:
-        if element in set_2:
-            common_elements.add(element)
-    return common_elements
+    return set_1 & set_2
