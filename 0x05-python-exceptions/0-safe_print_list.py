e293f7a2ada06f3cabcbc18e3116e53dd2010f60
Maryam Adegboyega <maryamshile@gmail.com>
2023-08-28 21:12:28 -0700

    created 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        new = ''.join(str(i) for i in my_list[:x])
+        count = 0
+        for n in new:
+            count += 1
+        print(new)
+        return(count)
+    except:
+        pass
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This task is on Python Exception fro error handling using try, except, finally, raise and pass.
